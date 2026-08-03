036a43077e9f8b18fbd00621c617f8c15c7000d2
EderLima88 <eder.ebl@gmail.com>
2026-08-03 07:31:41 -0300

    "Fix links projetos"

--- src/main/java/com/eder/portfolioapi/config/DatabaseSeeder.java
+++ src/main/java/com/eder/portfolioapi/config/DatabaseSeeder.java
@@ -4,6 +4,8 @@ import com.eder.portfolioapi.domain.*;
 import com.eder.portfolioapi.repository.PerfilRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.ArrayList; 
 import java.util.List;
 
@@ -17,6 +19,7 @@ public class DatabaseSeeder implements CommandLineRunner {
     }
 
     @Override
+    @Transactional //GARANTE QUE O SPRING SÓ ABRA A API APÓS O COMMIT TOTAL NO BANCO
     public void run(String... args) throws Exception {
         if (repository.count() == 0) {
             Perfil eder = new Perfil();
@@ -31,12 +34,12 @@ public class DatabaseSeeder implements CommandLineRunner {
             eder.setCompetencias(List.of("APIs RESTful", "Spring Boot", "Docker", "React", "Angular", "Java", "MySQL", "Shell Scripting", "Linux"));
 
             List<Projeto> projs = new ArrayList<>();
-            Projeto p1 = new Projeto(); p1.setTitulo("Projeto CRUD em Java para aplicação Desktop"); p1.setDescricao("Aplicação desktop desenvolvida em Java com interface gráfica, permitindo a manipulação de dados em um banco de dados SQLite."); p1.setLinkGithub("https://github.com/JavaCrudSqlite"); p1.setTags(List.of("Java", "Swing", "MySQL")); projs.add(p1);
-            Projeto p2 = new Projeto(); p2.setTitulo("Infraestrutura como Código (IaC) - Linux"); p2.setDescricao("Projeto de infra como código (IaC) para configuração de serviços e gerenciamento de usuários."); p2.setLinkGithub("https://github.com/linux-proj1-infra"); p2.setTags(List.of("Linux", "Shell Scripting", "Automação")); projs.add(p2);
-            Projeto p3 = new Projeto(); p3.setTitulo("API RESTful - Sistema de E-commerce"); p3.setDescricao("API completa para gerenciamento de produtos, carrinho de compras e pedidos dos usuários."); p3.setLinkGithub("https://github.com/react-docker-yarn-typescript-2"); p3.setTags(List.of("Docker", "TypeScript", "React")); projs.add(p3);
-            Projeto p4 = new Projeto(); p4.setTitulo("Aplicação Spring Boot com H2 Database"); p4.setDescricao("Aplicação desenvolvida com Spring Boot com padrão MVC"); p4.setLinkGithub("https://github.com/spring-rest-api-h2"); p4.setTags(List.of("Java", "Spring Boot", "MVC", "H2 Database")); projs.add(p4);
-            Projeto p5 = new Projeto(); p5.setTitulo("Aplicação gestão de pessoas com HTML, CSS e JavaScript puros"); p5.setDescricao("Manipuação de dados de pessoas com HTML, CSS e JavaScript puros, utilizando LocalStorage MYSQL."); p5.setLinkGithub("https://github.com/CRUDTargetTrust"); p5.setTags(List.of("HTML", "Bootstrap", "PHP", "MySQL")); projs.add(p5);
-            Projeto p6 = new Projeto(); p6.setTitulo("Blog em Angula no git pages sobre Silent Hill"); p6.setDescricao("Blog desenvolvido em Angular, hospedado no GitHub Pages, abordando informações e curiosidades sobre a franquia de jogos Silent Hill."); p6.setLinkGithub("https://github.com/angular-blog-jogos"); p6.setTags(List.of("Angular", "GitHub Pages", "Modular", "Routing")); projs.add(p6);
+            Projeto p1 = new Projeto(); p1.setTitulo("Projeto CRUD em Java para aplicação Desktop"); p1.setDescricao("Aplicação desktop desenvolvida em Java com interface gráfica, permitindo a manipulação de dados em um banco de dados SQLite."); p1.setLinkGithub("https://github.com/EderLima88/JavaCrudSqlite"); p1.setTags(List.of("Java", "Swing", "MySQL")); projs.add(p1);
+            Projeto p2 = new Projeto(); p2.setTitulo("Infraestrutura como Código (IaC) - Linux"); p2.setDescricao("Projeto de infra como código (IaC) para configuração de serviços e gerenciamento de usuários."); p2.setLinkGithub("https://github.com/EderLima88/linux-proj1-infra"); p2.setTags(List.of("Linux", "Shell Scripting", "Automação")); projs.add(p2);
+            Projeto p3 = new Projeto(); p3.setTitulo("API RESTful - Sistema de E-commerce"); p3.setDescricao("API completa para gerenciamento de produtos, carrinho de compras e pedidos dos usuários."); p3.setLinkGithub("https://github.com/EderLima88/react-docker-yarn-typescript-2"); p3.setTags(List.of("Docker", "TypeScript", "React")); projs.add(p3);
+            Projeto p4 = new Projeto(); p4.setTitulo("Aplicação Spring Boot com H2 Database"); p4.setDescricao("Aplicação desenvolvida com Spring Boot com padrão MVC"); p4.setLinkGithub("https://github.com/EderLima88/spring-rest-api-h2"); p4.setTags(List.of("Java", "Spring Boot", "MVC", "H2 Database")); projs.add(p4);
+            Projeto p5 = new Projeto(); p5.setTitulo("Aplicação gestão de pessoas com HTML, CSS e JavaScript puros"); p5.setDescricao("Manipuação de dados de pessoas com HTML, CSS e JavaScript puros, utilizando LocalStorage MYSQL."); p5.setLinkGithub("https://github.com/EderLima88/CRUDTargetTrust"); p5.setTags(List.of("HTML", "Bootstrap", "PHP", "MySQL")); projs.add(p5);
+            Projeto p6 = new Projeto(); p6.setTitulo("Blog em Angula no git pages sobre Silent Hill"); p6.setDescricao("Blog desenvolvido em Angular, hospedado no GitHub Pages, abordando informações e curiosidades sobre a franquia de jogos Silent Hill."); p6.setLinkGithub("https://github.com/EderLima88/angular-blog-jogos"); p6.setTags(List.of("Angular", "GitHub Pages", "Modular", "Routing")); projs.add(p6);
             eder.setProjetos(projs);
 
             List<Certificado> certs = new ArrayList<>();
@@ -46,7 +49,7 @@ public class DatabaseSeeder implements CommandLineRunner {
             Certificado c4 = new Certificado(); c4.setNome("Ciência de DadosCiência de Dados"); c4.setEmissor("Uniasselvi"); c4.setAno("2023"); c4.setUrl("https://drive.google.com/file/d/1sU3N4wjVvk8xln7mKe3nCufoOhw-SgFT/view?usp=drive_link"); certs.add(c4);
             eder.setCertificados(certs);
 
-            repository.save(eder);
+            repository.saveAndFlush(eder);
             System.out.println("🚀 Banco de dados PostgreSQL populado com sucesso com os dados do Eder!");
         }
     }
